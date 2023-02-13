acca0884a99944943eb3210992991b162de8fe9b
Dennis Hammer <dennis.hammer@hiq.se>
2023-02-13 16:22:50 +0100

    Refactor input types

--- 2023-02-13/typescript/game-of-life/test/game-of-life.test.ts
+++ 2023-02-13/typescript/game-of-life/test/game-of-life.test.ts
@@ -1,22 +1,24 @@
 import {describe, expect, it} from "vitest";
 
-type AliveCell = {
+type Cell = {
     neighbours: number
+    isAlive: boolean
 }
 
-function isAliveStayingAlive(aliveCell: AliveCell): boolean {
-    return aliveCell.neighbours === 2 || aliveCell.neighbours === 3;
+function isAliveStayingAlive(neighbours: number): boolean {
+    return neighbours === 2 || neighbours === 3;
 
 }
 
-function isDeadBecomingAlive(deadCell: DeadCell) {
-    return deadCell.neighbours === 3;
+function isDeadBecomingAlive(neighbours: number) {
+    return neighbours === 3;
 }
 
 describe("Rules", () => {
     it("Live cell with fewer than two neighbours dies", () => {
-        const liveCell = {
-            neighbours: 1
+        const liveCell: Cell = {
+            neighbours: 1,
+            isAlive: false
         };
 
         const alive = isAliveStayingAlive(liveCell);
@@ -25,8 +27,9 @@ describe("Rules", () => {
     });
 
     it.each([2, 3])("Any live cell with two or three live neighbours lives", (neighbours: number) => {
-        const liveCell = {
-            neighbours: neighbours
+        const liveCell: Cell = {
+            neighbours: neighbours,
+            isAlive: false
         };
 
         const alive = isAliveStayingAlive(liveCell);
@@ -36,8 +39,9 @@ describe("Rules", () => {
 
 
     it.each([4, 5])("Any live cell with more than three live neighbours dies", (neighbours) => {
-        const liveCell = {
-            neighbours: neighbours
+        const liveCell: Cell = {
+            neighbours: neighbours,
+            isAlive: false
         };
 
         const alive = isAliveStayingAlive(liveCell);
@@ -46,8 +50,9 @@ describe("Rules", () => {
     });
 
     it("Any dead cell with exactly three live neighbours becomes a live cell", () => {
-        const deadCell = {
-            neighbours: 3
+        const deadCell: Cell = {
+            neighbours: 3,
+            isAlive: false
         };
 
         const alive = isDeadBecomingAlive(deadCell);
@@ -56,8 +61,9 @@ describe("Rules", () => {
     });
 
     it("Any dead cell without three live neighbours stays dead", () => {
-        const deadCell = {
-            neighbours: 2
+        const deadCell: Cell = {
+            neighbours: 2,
+            isAlive: false
         };
 
         const alive = isDeadBecomingAlive(deadCell);
