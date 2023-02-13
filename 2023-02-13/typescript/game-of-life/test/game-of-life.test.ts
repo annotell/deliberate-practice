f0ecd316318c61f42c0f02c207132253af4ae010
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-02-13 10:59:03 +0100

    implement first test, add 2nd

--- 2023-02-13/typescript/game-of-life/test/game-of-life.test.ts
+++ 2023-02-13/typescript/game-of-life/test/game-of-life.test.ts
@@ -1,5 +1,9 @@
 import {describe, expect, it} from "vitest";
 
+function isDeadOrAlive(_cell: { neighbours: number }) {
+    return false;
+}
+
 describe("Rules", () => {
     it("Live cell with fewer than two neighbours dies", () => {
         const liveCell = {
@@ -7,7 +11,18 @@ describe("Rules", () => {
         };
 
         const deadOrAlive = isDeadOrAlive(liveCell);
+
         expect(deadOrAlive).toEqual(false);
+    });
+
+    it("Any live cell with two or three live neighbours lives on to the next generation.", () => {
+        const liveCell = {
+            neighbours: 2
+        };
+
+        const deadOrAlive = isDeadOrAlive(liveCell);
+
+        expect(deadOrAlive).toEqual(true);
     });
 });
 
