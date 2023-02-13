e5807dd5df0e4f430741f1b6692f6eb887b75890
Dennis Hammer <dennis.hammer@hiq.se>
2023-02-13 16:33:16 +0100

    Refactor tests

--- 2023-02-13/typescript/game-of-life/test/game-of-life.test.ts
+++ 2023-02-13/typescript/game-of-life/test/game-of-life.test.ts
@@ -14,8 +14,12 @@ function isDeadBecomingAlive(neighbours: number) {
     return neighbours === 3;
 }
 
+function checkCellLifeStatus(cell: Cell) {
+    return isAliveStayingAlive(cell.neighbours);
+}
+
 describe("Cell behaviour", () => {
-    it("A living cell with only one neighbour", () => {
+    it("A living cell with only one neighbour dies", () => {
         const liveCell: Cell = {
             neighbours: 1,
             isAlive: true
@@ -29,57 +33,38 @@ describe("Cell behaviour", () => {
 
 describe("Rules", () => {
     it("Live cell with fewer than two neighbours dies", () => {
-        const liveCell: Cell = {
-            neighbours: 1,
-            isAlive: false
-        };
+        const neighbours = 1;
 
-        const alive = isAliveStayingAlive(liveCell.neighbours);
+        const alive = isAliveStayingAlive(neighbours);
 
         expect(alive).toEqual(false);
     });
 
     it.each([2, 3])("Any live cell with two or three live neighbours lives", (neighbours: number) => {
-        const liveCell: Cell = {
-            neighbours: neighbours,
-            isAlive: false
-        };
-
-        const alive = isAliveStayingAlive(liveCell.neighbours);
+        const alive = isAliveStayingAlive(neighbours);
 
         expect(alive).toEqual(true);
     });
 
 
     it.each([4, 5])("Any live cell with more than three live neighbours dies", (neighbours) => {
-        const liveCell: Cell = {
-            neighbours: neighbours,
-            isAlive: false
-        };
-
-        const alive = isAliveStayingAlive(liveCell.neighbours);
+        const alive = isAliveStayingAlive(neighbours);
 
         expect(alive).toEqual(false);
     });
 
     it("Any dead cell with exactly three live neighbours becomes a live cell", () => {
-        const deadCell: Cell = {
-            neighbours: 3,
-            isAlive: false
-        };
+        const neighbours = 3;
 
-        const alive = isDeadBecomingAlive(deadCell.neighbours);
+        const alive = isDeadBecomingAlive(neighbours);
 
         expect(alive).toEqual(true);
     });
 
     it("Any dead cell without three live neighbours stays dead", () => {
-        const deadCell: Cell = {
-            neighbours: 2,
-            isAlive: false
-        };
-
-        const alive = isDeadBecomingAlive(deadCell.neighbours);
+        const neighbours = 2;
+            
+        const alive = isDeadBecomingAlive(neighbours);
 
         expect(alive).toEqual(false);
     });
