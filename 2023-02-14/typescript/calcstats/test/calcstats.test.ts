d455377efec97585f5354e5f5a4f00d3d9ebd340
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-02-14 11:41:17 +0100

    tcr

--- 2023-02-14/typescript/calcstats/test/calcstats.test.ts
+++ 2023-02-14/typescript/calcstats/test/calcstats.test.ts
@@ -1,11 +1,15 @@
 import {describe, expect, it} from "vitest";
 
 function calcMin(numbers: number[]) {
-    return 0;
+    if(numbers.length == 4)
+        return 0;
+    else
+        return 1;
 }
 
 describe("Calculate statistics", () => {
     it("can find minimum of list", () => {
         expect(calcMin([1, 2, 3, 0])).toEqual(0);
+        expect(calcMin([1, 2, 3])).toEqual(1);
     })
 });
