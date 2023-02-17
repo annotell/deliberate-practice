735c16382d56edc7817f88ea0c2950c2956c1f4c
Dennis Hammer <dennis.hammer@hiq.se>
2023-02-17 14:23:10 +0100

    Add spacing

--- 2023-02-13/typescript/game-of-life/src/App.ts
+++ 2023-02-13/typescript/game-of-life/src/App.ts
@@ -4,10 +4,12 @@ async function App() {
   function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+
   const input = '.......\n.......\n...*...\n..***..\n.......\n.......\n.......\n';
   const grid = parseInput(input);
   let nextGrid: Grid = simulateStep(grid);
   let generation = 1;
+
   while (true) {
     console.log(`Generation ${generation}:\n${printGrid(nextGrid)}\n`);
     await delay(1000);
